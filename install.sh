40d02418a3b55d785b00f318fff5dfed329f7f24
weihaoxuan <weihxa@126.com>
2017-11-24 13:28:40 +0800

    1

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ source /etc/profile
 
 
 echo '开始创建数据库及账户'
-sed i.bak "s/100.100.100.100/$mysqlip/" ./xbmanIntegrated/settings.py
+sed -i.bak "s/100.100.100.100/$mysqlip/" xbmanIntegrated/settings.py
 mysql -h $mysqlip -u$mysqlusername -p$mysqlpassword  <<EOF 2>/dev/null
     CREATE DATABASE aclsm CHARACTER SET utf8 COLLATE utf8_general_ci;
     grant all privileges on aclsm.* to aclsm@* identified by 'aclsm.com';
@@ -23,6 +23,6 @@ EOF
 echo '初始化数据库表和登陆账户'
 python manage.py makemigrations
 python manage.py migrate
-echo "from Integrated.models import UserProfile as User; User.objects.create_superuser('ACLSM', 'aclsm@aclsm.com', 'aclsm')" | python manage.py shell
+echo "from Integrated.models import UserProfile as User; User.objects.create_superuser('ACLSM', 'aclsm@aclsm.com', 'aclsm.com')" | python manage.py shell
 echo 'mysql登录账号密码为：aclsm/aclsm.com'
-echo '默认登录账号密码为：aclsm@aclsm.com/aclsm'
+echo '默认登录账号密码为：aclsm@aclsm.com/aclsm.com'
